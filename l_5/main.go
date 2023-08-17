fe06f26567d475948f7e8b5cf34dd261730d924f
ciscomsk <kuznetzoff@gmail.com>
2023-08-17 10:01:42 +0300

    Added github module

--- l_5/main.go
+++ l_5/main.go
@@ -11,7 +11,7 @@ func main() {
 	//fmt.Println(a + b) // v1 - local
 
 	// v2 - local package - import "sum/util"
-	// v3 - github repo
+	// v3 - github repo - import ""github.com/ciscomsk/otus_golang_basic_l5_util/util"
 	c := util.Sum(a, b)
 	fmt.Println(c)
 }
